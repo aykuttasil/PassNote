a2c2cff025a60de0d5ac4945a2e346619bb154ea
Aykut Asil <aykuttasil@gmail.com>
2018-06-30 19:38:53 +0300

    update

--- app/src/main/java/aykuttasil/com/passnote/ui/main/MainActivity.kt
+++ app/src/main/java/aykuttasil/com/passnote/ui/main/MainActivity.kt
@@ -77,56 +77,6 @@ class MainActivity : BaseActivity() {
             }
             binding.executePendingBindings()
         })
-
-
-        btnImagePicker.setOnClickListener {
-            val intent = Intent(Intent.ACTION_GET_CONTENT).also { it.type = "image/*" }
-            startActivityForResult(intent, IMAGE_PICK_REQUEST)
-        }
-
-        btnShowNotif.setOnClickListener {
-            NewMessageNotification.notify(this, "Test", 1)
-        }
-
-        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_IMAGE_URI)) {
-            logd { "Saved state contains an image Uri. Let's assign it to imageUri." }
-            imageUri = savedInstanceState.getParcelable(KEY_IMAGE_URI) as Uri
-            imageUri?.let { loadAndShowPhoto(it) }
-        }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == IMAGE_PICK_REQUEST && resultCode == Activity.RESULT_OK && data != null) {
-            logd { "User picked photo with uri ${data.data}, let's load it" }
-            imageUri = data.data // So we can store it in onSaveInstanceState
-            loadAndShowPhoto(data.data)
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        if (outState != null && imageUri != null) {
-            logd { "Let's save the imageUri to our saved state so we can load it when we come back!" }
-            outState.putParcelable(KEY_IMAGE_URI, imageUri)
-        }
-    }
-
-
-    private fun loadAndShowPhoto(uri: Uri) {
-        load {
-            // This run on a background thread
-            logd { "Start loading image on thread ${Thread.currentThread().name}" }
-            delay(2000L) // Fake a long loading so we can test what happens in onStop()
-            MediaStore.Images.Media.getBitmap(contentResolver, uri)
-        } then {
-            // This runs on the main thread
-            logd { "Image with size ${it.width} x ${it.height} loaded. Display on ImageView running on thread ${Thread.currentThread().name}" }
-            imageView.setImageBitmap(it)
-        }
-    }
-
-    // override fun onSupportNavigateUp() = Navigation.findNavController(R.id.nav_host_fragment).navigateUp()
 
 }
