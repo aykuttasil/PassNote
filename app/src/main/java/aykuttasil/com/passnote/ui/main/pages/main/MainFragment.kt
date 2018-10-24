be5d61d825079dd90db0278a2ea205973f738523
Aykut Asil <aykuttasil@gmail.com>
2018-10-25 01:01:47 +0300

    ui update

--- app/src/main/java/aykuttasil/com/passnote/ui/main/pages/main/MainFragment.kt
+++ app/src/main/java/aykuttasil/com/passnote/ui/main/pages/main/MainFragment.kt
@@ -5,9 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
 import aykuttasil.com.passnote.R
-import aykuttasil.com.passnote.data.Storage
 import aykuttasil.com.passnote.databinding.FragmentMainBinding
 import aykuttasil.com.passnote.di.Injectable
 import aykuttasil.com.passnote.ui.common.BaseSecureFragment
@@ -19,7 +17,6 @@ import org.jetbrains.anko.sdk25.coroutines.onClick
 class MainFragment : BaseSecureFragment(), Injectable {
 
     private val binding: FragmentMainBinding by Inflate(R.layout.fragment_main)
-    private val storage: Storage by lazy(LazyThreadSafetyMode.NONE) { Storage(context!!.applicationContext) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return binding.root
@@ -27,31 +24,8 @@ class MainFragment : BaseSecureFragment(), Injectable {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btnGoUserActivity.onClick {
-            Navigation.findNavController(binding.btnGoUserActivity).navigate(R.id.action_mainFragment_to_aboutFragment)
-        }
-
-        btnImagePicker.onClick {
-            startActivity(Intent(activity!!.applicationContext, NoteListActivity::class.java))
-            //x()
+        btnNoteList.onClick {
+            startActivity(Intent(getContext()?.applicationContext, NoteListActivity::class.java))
         }
     }
-
-
-    /*
-    fun x() {
-        val dialog = AuthenticationDialog()
-        if (storage.isFingerprintAllowed() && systemServices.hasEnrolledFingerprints()) {
-            dialog.cryptoObjectToAuthenticateWith = EncryptionServices(context!!).prepareFingerprintCryptoObject()
-            dialog.fingerprintInvalidationListener = { onFingerprintInvalidation(it) }
-            dialog.fingerprintAuthenticationSuccessListener = { validateKeyAuthentication(secret, it) }
-            if (dialog.cryptoObjectToAuthenticateWith == null) dialog.stage = AuthenticationDialog.Stage.NEW_FINGERPRINT_ENROLLED else dialog.stage = AuthenticationDialog.Stage.FINGERPRINT
-        } else {
-            dialog.stage = AuthenticationDialog.Stage.PASSWORD
-        }
-        dialog.authenticationSuccessListener = { startSecretActivity(ADD_SECRET_REQUEST_CODE, SecretActivity.MODE_VIEW, it, secret) }
-        dialog.passwordVerificationListener = { validatePassword(it) }
-        dialog.show(childFragmentManager, "Authentication")
-    }
-    */
 }
